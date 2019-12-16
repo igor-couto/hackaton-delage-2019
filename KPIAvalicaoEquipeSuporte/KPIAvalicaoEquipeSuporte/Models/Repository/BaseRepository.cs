65ed546c3f66f4e78d84d22f09f30fb156c88e1b
Luiz Fernando <luizfernandobeninidoamaral@gmail.com>
2019-12-16 12:04:29 -0300

    BaseRepository Criado

---
+++ KPIAvalicaoEquipeSuporte/KPIAvalicaoEquipeSuporte/Models/Repository/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace KPIAvalicaoEquipeSuporte.Models.Repository
+{
+    public abstract class BaseRepository
+    {
+        private readonly IConfiguration _config;
+        protected SqlConnection _conn { get { return new SqlConnection(_config.GetConnectionString("SqlConnection")); } }
+
+    }
+}
