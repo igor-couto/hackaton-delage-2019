b108cdae42354d7e5d2349c7f1291ec68b1c7d65
Luiz Fernando <luizfernandobeninidoamaral@gmail.com>
2019-12-16 16:48:56 -0300

    Criando Primeira consulta no banco

--- KPIAvalicaoEquipeSuporte/KPIAvalicaoEquipeSuporte/Models/Repository/AtendimentoRepository.cs
+++ KPIAvalicaoEquipeSuporte/KPIAvalicaoEquipeSuporte/Models/Repository/AtendimentoRepository.cs
@@ -14,8 +14,14 @@ namespace KPIAvalicaoEquipeSuporte.Models.Repository
             var parameters = new DynamicParameters();
             parameters.Add("IdAnalista", IdAnalista, DbType.Int32);
 
-            var sql = @"SELECT 
-";
+            var sql = @"SELECT  
+                                
+                            FROM funcionario f (NOLOCK)
+                                INNER JOIN nivel_funcionario_setor nfs (NOLOCK) on 
+                                INNER JOIN atendimento a (NOLOCK) on 
+                                INNER JOIN def_nivel dn (NOLOCK) on 
+                                INNER JOIN def_setor ds (NOLOCK) on
+                            WHERE a.idAtendimento = @IdAnalista";
 
             using (_conn)
             {
