f801e1f0b7f38943de08fc6a5779d9daca663df1
Igor Couto <igor.fcouto@gmail.com>
2019-12-16 17:36:45 -0300

    Adicionado Tipo do Grafico

--- KPIAvalicaoEquipeSuporte/KPIAvalicaoEquipeSuporte/Models/ViewModel/FiltroViewModel.cs
+++ KPIAvalicaoEquipeSuporte/KPIAvalicaoEquipeSuporte/Models/ViewModel/FiltroViewModel.cs
@@ -5,7 +5,7 @@ namespace KPIAvalicaoEquipeSuporte.Models.ViewModel
     public class FiltroViewModel
     {
         public string Analista { get; set; }
-        public string Situacao { get; set; }
+        public string TipoDoGrafico { get; set; }
         public DateTime DataInicio { get; set; }
         public DateTime DataFim { get; set; }
     }
